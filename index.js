3ba532ae7d87f0e943972e814a114a4c6a0313a7
Vachet Virginie <vachetvirginie@gmail.com>
2017-08-24 15:18:06 +0200

    configure the template engine of Express

--- index.js
+++ index.js
@@ -1,37 +1,24 @@
 const fs = require('fs');
 const express = require('express');
 const mustache = require('mustache');
-
-
-
-
 let app = express();
 
-app.get("/template", function(req, resp) {
-    let str = mustache.render("Bienvenue {{name}}!!! Bien joué mm!", {
-        name: "vivi"
-    })
-    resp.send(str)
+
+var mustacheExpress = require('mustache-express');
+
+// Register '.html' extension with The Mustache Express
+app.engine('html', mustacheExpress());
+
+app.set('view engine', 'mustache');
+app.set('template', __dirname + '/template'); // you can change '/views' to '/public',
+// but I recommend moving your templates to a directory
+// with no outside access for security reasons
+
+app.get('/', function(req, res) {
+    res.render('index.html');
 });
 
 
-app.engine("html", function(path, options, callback) {
-    fs.readFile(path, function(err, content) {
-        if (err) {
-            console.error("fail to open template:", err);
-            return callback(err);
-        }
-        let str = mustache.render(content.toString(), options);
-        return callback(null, str);
-    })
-});
-
-// specify the views directory
-app.set('views', './template');
-// register the template engine
-app.set('view engine', 'html');
-
-app.use(express.static("public"));
 
 app.listen(8082, function() {
     console.log('Server listening on port 80...');
